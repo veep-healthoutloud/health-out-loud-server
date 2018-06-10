ca0ebef38edd2bc1eb57aa010f2acdc99729a539
Rathu Kugan <rathu.kugan@gmail.com>
2018-06-10 13:14:20 -0400

    Updated query param in checkIfQueryExists function

--- passport.js
+++ passport.js
@@ -7,6 +7,7 @@ const passportJWT = require('passport-jwt');
 const JWTStrategy   = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 
+//Grab DB connection
 var dbConnection = require('./db_connection');
 var db;
 dbConnection.connectToServer(function(err) {
@@ -20,31 +21,8 @@ passport.use('local', new LocalStrategy({
         session: false
     }, 
     function (email, password, done) {
-    	//Make sure user is verified, unverified users must not get a JWT
-	 //  	db.collection('unverified').count({ email: email }, function (err, count){ 
-	 //    	if(count > 0) {
-	 //        	return done(null, false, {error: 'User is unverified'});
-	 //    	}
-		// }); 
-
-		// db.collection('user').findOne({ email: email }, function (err, result) { 
-		// 	if (!result) {
-		// 		return done(null, false, {error: 'Incorrect email or password.'}); //User doesnt exist
-		// 	}
-		// 	//User exists so validate password using salt/hash
-		// 	var user = new User(result.email);
-		// 	user.setSalt(result.salt);
-		// 	user.setHash(result.hash);
-
-		// 	if (!user.validatePassword(password)) { //Validate with user supplied password
-		// 		return done(null, false, {error: 'Incorrect email or password.'});
-		// 	}
-
-		// 	//Password valid so return user object
-		// 	return done(null, user, {success: 'Logged In Successfully'});	
-		// });  
-
-		checkIfQueryExists(db, "unverified", email, function(isNotVerified) {
+    	//Check if user is in unverified collection, otherwise find the user set credentials
+		checkIfQueryExists(db, "unverified", {email: email}, function(isNotVerified) {
 		    if(isNotVerified) { //return with error since unverified users must not get a JWT
 		    	return done(null, false, {error: 'User is unverified'});
 		    }
@@ -83,10 +61,9 @@ passport.use(new JWTStrategy({
 
 //For example check if a user exists in unverified collection to determine if user is verified or not
 //Has a callback (onDone) so you can check result and then synchrously execute another db call
-
-// EMAIL:query, email should be a variable
+//query variable is {param: value}
 const checkIfQueryExists = function(dbConnection, collectionName, query, onDone) {
-	db.collection(collectionName).count({ email: query }, function (err, count){ 
+	db.collection(collectionName).count(query, function (err, count){ 
     	if (onDone) onDone((count > 0)); //Call the callback with results of the exists check
 	}); 
 };
